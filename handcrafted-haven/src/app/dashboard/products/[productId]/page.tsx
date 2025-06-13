cd816cc6cb4a6f9f465d6f5749d038d59cd477e2
baileynance <baieyj.nance@gmail.com>
2025-06-12 21:47:13 -0700

    more

--- handcrafted-haven/src/app/dashboard/products/[productId]/page.tsx
+++ handcrafted-haven/src/app/dashboard/products/[productId]/page.tsx
@@ -1,10 +1,8 @@
-type PageProps = {
-    params: {
-      productId: string;
-    };
-  };
-  
-  export default function ProductPage({ params }: PageProps) {
+export default function ProductPage({
+    params,
+  }: {
+    params: { productId: string };
+  }) {
     return <div>Product ID: {params.productId}</div>;
-  }
+}
   
